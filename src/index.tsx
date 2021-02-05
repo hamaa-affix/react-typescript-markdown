bc7c016199c3a0c116918d10f16937f664cc43a8
hamaa-affix <59754313+hamaa-affix@users.noreply.github.com>
2021-02-05 17:32:48 +0900

    カスタムフックを用いたlocalStorageへの保存処理

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,22 @@
 import { render } from "react-dom";
-
+import { createGlobalStyle } from "styled-components";
 import App from "./App";
+import Editer from "./pages/editor";
 
-const rootElement = document.getElementById("root");
-render(<App />, rootElement);
+// const rootElement = document.getElementById("root");
+// render(<App />, rootElement);
+
+const GlobalStyle = createGlobalStyle`
+    body * {
+      box-sizing: border-box;
+    }
+  `;
+
+const Main = (
+  <>
+    <GlobalStyle />
+    <Editer />
+  </>
+);
+
+render(Main, document.getElementById("root"));
